cad5b92a44a1e9f362fb8bc7980d48a63e20fa77
José Thomaz <jose.thoomaz@gmail.com>
2025-05-07 10:42:43 -0300

    First commit

---
+++ src/test/java/com/project/Avoar/AvoarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Avoar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvoarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
