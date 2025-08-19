e7fc2a0c0c22c2d0f0429bef215fb125c1fb364a
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-08-19 02:23:40 -0300

    feat: mapper user to dto or dto to entity

---
+++ src/main/java/com/project/Avoar/mapper/UserMapper.java
@@ -0,0 +1,23 @@
+package com.project.Avoar.mapper;
+
+import com.project.Avoar.dto.request.UserRequestDTO;
+import com.project.Avoar.dto.response.UserResponseDTO;
+import com.project.Avoar.entity.User;
+
+public class UserMapper {
+    public static UserResponseDTO toDTO(User user) {
+        if (user == null) return null;
+
+        return new UserResponseDTO(user.getName());
+    }
+
+    public static User toEntity(UserRequestDTO dto) {
+        if (dto == null) return null;
+
+        User user = new User();
+        user.setName(dto.username());
+        user.setEmail(dto.email());
+        user.setPassword(dto.password());
+        return user;
+    }
+}
