8b1fdfee50af79946867e7d38997c753a8529770
Filipe Kevyn <FilipeKevyn4@gmail.com>
2025-08-19 02:26:10 -0300

    feat: pageble for flights

--- src/main/java/com/project/Avoar/repository/FlightRepository.java
+++ src/main/java/com/project/Avoar/repository/FlightRepository.java
@@ -2,6 +2,7 @@ package com.project.Avoar.repository;
 
 import com.project.Avoar.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 }
